87bf8c13efb874976b8b739dc04adb719e4ec55d
thundermiracle <cissoliu@gmail.com>
2023-05-06 12:10:24 +0900

    feat: implement getInitialProps version

---
+++ apps/next-getinitialprops/.next/server/next-font-manifest.js
@@ -0,0 +1,14 @@
+self.__NEXT_FONT_MANIFEST={
+  "pages": {
+    "/": [
+      "static/media/2aaf0723e720e8b9-s.p.woff2"
+    ]
+  },
+  "app": {
+    "/Users/liufeng/Study/next-performance/node_modules/.pnpm/next@13.4.1_react-dom@18.2.0_react@18.2.0/node_modules/next/dist/build/webpack/loaders/css-loader/src/index.js??ruleSet[1].rules[1].oneOf[3].use[1]!/Users/liufeng/Study/next-performance/node_modules/.pnpm/next@13.4.1_react-dom@18.2.0_react@18.2.0/node_modules/next/dist/build/webpack/loaders/next-font-loader/index.js??ruleSet[1].rules[1].oneOf[3].use[2]!/Users/liufeng/Study/next-performance/node_modules/.pnpm/next@13.4.1_react-dom@18.2.0_react@18.2.0/node_modules/next/font/google/target.css?{\"path\":\"src/pages/index.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}": [
+      "static/media/2aaf0723e720e8b9-s.p.woff2"
+    ]
+  },
+  "appUsingSizeAdjust": true,
+  "pagesUsingSizeAdjust": true
+}
---
+++ apps/next-getinitialprops/src/pages/_app.tsx
@@ -0,0 +1,31 @@
+import "@/styles/globals.css";
+import type { AppContext, AppProps } from "next/app";
+import App from "next/app";
+
+interface MyProps extends AppProps {
+  host: string;
+  data: any;
+}
+
+export default function MyApp({ Component, pageProps, host, data }: MyProps) {
+  console.log({ host, data });
+
+  return <Component {...pageProps} />;
+}
+
+// use getInitialProps to fetch data
+MyApp.getInitialProps = async (appContext: AppContext) => {
+  const appProps: any = await App.getInitialProps(appContext);
+  const { req, res } = appContext.ctx;
+
+  if (req && res) {
+    const host = req.headers.host;
+    const dataRes = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+
+    return { pageProps: appProps.pageProps, host, data: await dataRes.json() };
+  } else {
+    const { host, data } = window.__NEXT_DATA__.props;
+
+    return { pageProps: appProps.pageProps, host, data };
+  }
+};
